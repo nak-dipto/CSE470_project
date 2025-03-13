1144a74b79926db83cb7f36e2eed83ac9002f46c
nak-dipto <nak.dipto@gmail.com>
2025-03-14 03:11:46 +0600

    Add homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,62 @@
-import Image from "next/image";
-
+"use client";
+import { FaBagShopping } from "react-icons/fa6";
+import { useSession } from "next-auth/react";
 export default function Home() {
+  const { data: session } = useSession();
+  let currentUserRole = session?.user.role;
+  console.log(session);
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+        <h1 className="scroll-m-20 text-5xl font-extrabold tracking-tight lg:text-5xl">
+          Hello{session ? ` ${session.user.name}` : ""},
+          <br />
+          Welcome to ByteBuy
+        </h1>
+        <p className="text-justify">
+          Welcome to ByteBuy, your ultimate destination for high-quality
+          computer parts and accessories. Whether you’re a seasoned gamer, a
+          tech enthusiast, or building your own custom rig, we’ve got everything
+          you need to power up your system. From cutting-edge processors to the
+          latest graphics cards, memory, storage, and more, ByteBuy offers a
+          wide selection of trusted brands at competitive prices. Shop with
+          confidence and experience fast shipping, expert advice, and unbeatable
+          customer service. At ByteBuy, we make building your dream PC easy!
+        </p>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            href={
+              currentUserRole === "USER"
+                ? "/shop"
+                : currentUserRole === "ADMIN"
+                ? "/admin"
+                : "/signup"
+            }
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
+            {currentUserRole === "ADMIN" ? (
+              <>
+                <FaUserShield className="dark inline align-middle" size={20} />
+                Go to Admin Panel
+              </>
+            ) : (
+              <>
+                <FaBagShopping className="dark inline align-middle" size={20} />
+                Shop now
+              </>
+            )}
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            href="/about"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            About Us
           </a>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
